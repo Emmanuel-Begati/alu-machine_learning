cb502f4dabb4408412a4e743e6b4e7f95ba4537e
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-05-19 01:47:13 +0200

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -17,11 +17,3 @@ def matrix_shape(matrix):
         current_dim = current_dim[0] if current_dim else None
 
     return shape
-
-# Example usage
-mat1 = [[1, 2], [3, 4]]
-print("Shape of mat1:", matrix_shape(mat1))
-
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print("Shape of mat2:", matrix_shape(mat2))
