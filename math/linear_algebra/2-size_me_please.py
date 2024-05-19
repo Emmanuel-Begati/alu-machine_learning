ad6d0cf471e6073d147729d7d9cd96b2eac4fabc
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-05-19 02:44:23 +0200

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -10,16 +10,6 @@ def matrix_shape(matrix):
     Returns:
         list: A list of integers representing the shape of the matrix. Each
               integer corresponds to the size of the matrix in that dimension.
-    
-    Example:
-        >>> matrix_shape([[1, 2], [3, 4]])
-        [2, 2]
-        
-        >>> matrix_shape([
-                [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-                [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-            ])
-        [2, 3, 5]
     """
 
     shape = []
