231c4e3e2f6d5b12340a5406980936fa0b3a7b75
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-05-19 02:40:33 +0200

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    """
-    Calculates the transpose of a given 2D matrix.
+        """
+    Calculates the shape of a given matrix.
 
     Args:
-        matrix (list[list]): A 2D matrix (nested list).
+        matrix (list): A nested list representing a matrix. All sublists in the
+                       same dimension are assumed to be of the same size/shape.
 
     Returns:
-        list[list]: The transposed matrix.
+        list: A list of integers representing the shape of the matrix. Each
+              integer corresponds to the size of the matrix in that dimension.
+    
+    Example:
+        >>> matrix_shape([[1, 2], [3, 4]])
+        [2, 2]
+        
+        >>> matrix_shape([
+                [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+                [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
+            ])
+        [2, 3, 5]
     """
+
     shape = []
     current_dim = matrix
 
