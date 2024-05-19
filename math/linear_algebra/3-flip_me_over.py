b7fc083a9878cff41b34c9d224ce6543c8e2cc42
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-05-19 02:08:37 +0200

    done

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -11,17 +11,3 @@ def matrix_transpose(matrix):
             result[j][i] = matrix[i][j]
 
     return result
-
-# Example usage
-mat1 = [[1, 2], [3, 4]]
-print("Original matrix:")
-print(mat1)
-print("Transposed matrix:")
-print(matrix_transpose(mat1))
-
-mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print("Original matrix:")
-print(mat2)
-print("Transposed matrix:")
-print(matrix_transpose(mat2))
