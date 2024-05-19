4faa92f806f2e3fac1a0c3f914cf0e211e8b79c1
Emmanuel-Begati <e.yakubu@alustudent.com>
2024-05-19 03:26:33 +0200

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,16 +5,13 @@ def matrix_shape(matrix):
 
     Args:
         matrix (list): A nested list representing a matrix. All sublists in the
-                       same dimension are assumed to be of the same size/shape.
-
+                       same dimension are assumed to be of the same size/shape
     Returns:
         list: A list of integers representing the shape of the matrix. Each
-              integer corresponds to the size of the matrix in that dimension.
-    
+              integer corresponds to the size of the matrix in that dimension. 
     Example:
         >>> matrix_shape([[1, 2], [3, 4]])
         [2, 2]
-        
         >>> matrix_shape([
                 [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
                 [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
