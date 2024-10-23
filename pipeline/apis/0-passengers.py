4cd06dbf320c3d5440e32f189952ebb05f64726f
Emmanue-Begati <e.yakubu@alustudent.com>
2024-10-23 11:26:24 +0200

    debuggin

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to retrieve a list of starships from the SWAPI API
+that can hold a given number of passengers.
+"""
+
 import requests
 
 def availableShips(passenger_count):
+    """
+    Returns a list of starships that can hold at least `passenger_count` passengers.
+    
+    Args:
+        passenger_count (int): The minimum number of passengers the starship must hold.
+    
+    Returns:
+        list: A list of starship names that meet the passenger count criteria.
+    """
     url = 'https://swapi-api.alx-tools.com/api/starships'
     ships_list = []
     
@@ -20,9 +34,3 @@ def availableShips(passenger_count):
         url = data['next']
     
     return ships_list
-
-# Test the function
-if __name__ == "__main__":
-    ships = availableShips(4)
-    for ship in ships:
-        print(ship)
